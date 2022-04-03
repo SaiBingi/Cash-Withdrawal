edb936e97d05d2df82aaaf740e8834a02436b755
Sai Bingi <saibingi1208@gmail.com>
2022-04-03 12:31:19 +0000

    Cash Withdrawal Solution

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const DenominationItem = props => {
+  const {denomination, amountWithdraw} = props
+  const {value} = denomination
+
+  const withdrawalAmount = () => {
+    amountWithdraw(value)
+  }
+
+  return (
+    <li>
+      <button type="button" className="button" onClick={withdrawalAmount}>
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
